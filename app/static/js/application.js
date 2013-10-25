78511530f125551efe0fd10b76d83017252a0054
Joe Meissler <joe.meissler@gmail.com>
2013-10-24 17:21:07 -0700

    add movie ids

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -18,10 +18,19 @@ $(document).ready(function(){
     $('body').on('click', '.theater-link', function(){
         id = $(this).attr('id');
         console.log(id)
-        console.log(theaters[id]);
-        
+        theater = theaters[id];
+        for (var i=0; i<theater['movies'].length; i++){
+            theater['movies'][i]['id'] = i
+        }
+        console.log(theater);
+        $('#container').html(Mustache.render($('#theater').html(), theater));
         // Once user clicks on theater, show movies playing (js and mustache)
         // Prompt user to select movies (checkboxes)
         // Once submitted, graph movies (find JS library)
     });
+    var selected = new Array();
+    $('body').on('click', 'input', function(){
+        console.log('input checked');
+        
+    })
 });
