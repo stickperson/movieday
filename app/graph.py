19b6ad7fda306ed036bb747c79a1fd29371d61f4
stickperson <joe.meissler@gmail.com>
2013-10-31 14:05:06 -0700

    updated weight calculation

--- app/graph.py
+++ app/graph.py
@@ -1,3 +1,6 @@
+from datetime import date, datetime, time, timedelta
+
+
 class Vertex(object):
     def __init__(self, movie, showtime):
         self.neighbors = {}
@@ -6,16 +9,29 @@ class Vertex(object):
         self.start = showtime[0]
         self.end = showtime[1]
 
-    def different_movie(self, neighbor):
-        if neighbor['id'] == self.id:
+    # Returns weight in seconds. We are not passing in a day, so if the next movie starts before a movie ends
+    # this method would still return a positive number 
+    # e.g. self vertex ends at 1:00 and neighbor starts at 12:30. This would return 23:30
+    # Set cutoff to 12 hours for now
+    def calc_weight_seconds(self, neighbor):
+        fmt = '%H:%M'
+        time = datetime.strptime(neighbor.start, fmt) - datetime.strptime(self.end, fmt)
+        seconds = time.seconds
+        hours = seconds//3600
+        if hours > 12:
             return False
-        else:
-            return True
+        return seconds
+
+        def different_movie(self, neighbor):
+            if neighbor['id'] == self.id:
+                return False
+            else:
+                return True
 
     # will fix weight calculation
     def add_neighbor(self, neighbor):
         if different_movie():
-            weight = self.end - neighbor.start
+            weight = calc_weight(self, neighbor)
             self.neighbors[neighbor] = weight
 
 
@@ -37,20 +53,23 @@ class Graph(object):
             vertex = self.add_vert(end_vert)
         self.vert_list[start_vert].add_neighbor(end_vert)
 
+# Sample movie list
+# movies = [{
+#     'id': 0,
+#     'name': 'Movie 1',
+#     'showtimes':[(1200, 1330), (1245, 1415), (1330, 1500)]
+# }, {
+#     'id': 1,
+#     'name': 'Movie 2',
+#     'showtimes': [(1240, 1400),(1340, 1500)]
+# }]
 
-movies = [{
-    'id': 0,
-    'name': 'Movie 1',
-    'showtimes':[(1200, 1330), (1245, 1415), (1330, 1500)]
-}, {
-    'id': 1,
-    'name': 'Movie 2',
-    'showtimes': [(1240, 1400),(1340, 1500)]
-}]
+# Sample graph
+# g = Graph()
+# for movie in movies:
+#     for i in range(0,len(movie['showtimes'])):
+#         start = movie['showtimes'][i][0]
+#         end = movie['showtimes'][i][1]
+#         g.add_vert(movie, movie['showtimes'][i])
 
-g = Graph()
-for movie in movies:
-    for i in range(0,len(movie['showtimes'])):
-        start = movie['showtimes'][i][0]
-        end = movie['showtimes'][i][1]
-        g.add_vert(movie, movie['showtimes'][i])
+
