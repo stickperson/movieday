7e3eaddc1277f0e595adc4ced610b7a3d6ffb55c
stickperson <joe.meissler@gmail.com>
2013-11-01 14:47:07 -0700

    changed neighbors to children

--- app/graph.py
+++ app/graph.py
@@ -3,7 +3,7 @@ from datetime import date, datetime, time, timedelta
 
 class Vertex(object):
     def __init__(self, movie, showtime):
-        self.neighbors = {}
+        self.children = {}
         self.id = movie['id']
         self.name = movie['name']
         self.start = showtime[0]
@@ -12,32 +12,33 @@ class Vertex(object):
 
     # Returns weight in seconds. We are not passing in a day, so if the next movie starts before a movie ends
     # this method would still return a positive number (because of datetime)
-    # e.g. self vertex ends at 1:00 and neighbor starts at 12:30. This would return 23:30
+    # e.g. self vertex ends at 1:00 and child starts at 12:30. This would return 23:30
     # Set cutoff to 12 hours for now
-    def calc_weight(self, neighbor):
+    def calc_weight(self, child):
         print 'calculating weight'
         fmt = '%H:%M'
-        t = datetime.strptime(neighbor.start, fmt) - datetime.strptime(self.end, fmt)
+        t = datetime.strptime(child.start, fmt) - datetime.strptime(self.end, fmt)
         seconds = t.seconds
         hours = seconds//3600
         if hours > 12:
             return False
-        print '{} has a neighbor {} whose weight is {}'.format(self.name, neighbor.name, seconds)
+        print '{} has a child {} whose weight is {}'.format(self.name, child.name, seconds)
         return seconds
 
-    def different_movie(self, neighbor):
-        if neighbor.id == self.id:
+    def different_movie(self, child):
+        if child.id == self.id:
             print 'same movie'
             return False
         else:
             print 'different movie!'
             return True
 
-    def add_neighbor(self, neighbor):
-        print 'adding neighbor'
-        if self.different_movie(neighbor):
-            weight = self.calc_weight(neighbor)
-            self.neighbors[neighbor] = weight
+    def add_child(self, child):
+        print 'adding child'
+        if self.different_movie(child):
+            if self.calc_weight(child):
+                weight = self.calc_weight(child)
+                self.children[child] = weight
 
 
 class Graph(object):
@@ -47,18 +48,14 @@ class Graph(object):
 
     def add_vert(self, movie, showtime):
         vertex = Vertex(movie, showtime)
-        # unique_key = '{} - {}'.format(movie['id'], showtime[0])
         self.vert_list[vertex.unique_key] = vertex
-        # Rethink following line. What if there are duplicate keys?
-        # self.vert_list[showtime[0]] = vertex
         self.num_verts += 1
         return vertex
 
     def add_edge(self, start_vert, end_vert):
 
         # Duplicate keys may be a problem here.
-        # self.vert_list[start_vert.start].add_neighbor(end_vert)
-        self.vert_list[start_vert.unique_key].add_neighbor(end_vert)
+        self.vert_list[start_vert.unique_key].add_child(end_vert)
 
 # Sample movie list
 # movies = [{
