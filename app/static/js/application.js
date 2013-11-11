3aff894803f79e2694a6885ef9d8c78e76a4c35c
stickperson <joe.meissler@gmail.com>
2013-11-11 14:36:34 -0800

    formatted all modals the same

--- app/static/js/application.js
+++ app/static/js/application.js
@@ -210,6 +210,8 @@ function showSelected(choices){
         console.log(movies[results[0]['first_id']]);
         results[0]['first_poster'] = movies[results[0]['first_id']]['poster'];
         results[0]['second_poster'] = movies[results[0]['second_id']]['poster'];
+        results[0]['first_synopsis'] = movies[results[0]['first_id']]['synopsis'];
+        results[0]['second_synopsis'] = movies[results[0]['second_id']]['synopsis'];
         results[0]['first_score'] = movies[results[0]['first_id']]['score'];
         results[0]['second_score'] = movies[results[0]['second_id']]['score'];
         console.log(results);
